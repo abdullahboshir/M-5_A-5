11b57526ed6766bf7c3e6d458b4a978a6c37961e
abdullahboshir <abdullahboshir@gmao.com>
2022-02-22 00:03:48 +0600

    third commit

--- js/Assignment-5.js
+++ js/Assignment-5.js
@@ -1,40 +1,38 @@
-function moneyMaster() {
-    const foodInput = document.getElementById("food-cost").value;
-    const foodCostNumber = parseInt(foodInput);
-}
-
-
-
-
-document.getElementById("calculation-btn").addEventListener("click", function () {
-    const foodInput = document.getElementById("food-cost").value;
-    const foodCostNumber = parseInt(foodInput);
-    const rentInput = document.getElementById("rent-cost").value;
-    const rentCostNumber = parseInt(rentInput);
-    const clothesInput = document.getElementById("clothes-cost").value;
-    const clothesCostNumber = parseInt(clothesInput);
-    const total = foodCostNumber + rentCostNumber + clothesCostNumber;
-    
-
-    const balance = document.getElementById("total-balance").innerText = 10000 - total;
+function moneyMaster(item) {
+    const foodInput = document.getElementById(item + "-cost").value;
+    itemCost = parseInt(foodInput);
+    const total = itemCost + itemCost + itemCost;
 
 
     if (total > 0) {
         const totalExpensesText = document.getElementById("total-expenses").innerText = total; 
     }
+    return balance = document.getElementById("total-balance").innerText = 10000 - total;
+};
+
+
+
+
+document.getElementById("calculation-btn").addEventListener("click", function () {
+    
+    moneyMaster("food");
+    moneyMaster("rent");
+    moneyMaster("clothes");
+
+   
 });
 
 
-
-
-document.getElementById("save-button").addEventListener("click", function () {
+function savingAmount() {
     const savingInput = document.getElementById("saving-input").value;
     const savingAmount = document.getElementById("saving-amount").innerText = 10000 / 100 * savingInput;
 
     // remaining balance 
-    const remainingBalance = document.getElementById("remaining-balance").innerText = 10000 - savingAmount;
+    const remainingBalance = document.getElementById("remaining-balance").innerText = savingAmount;
+    
+};
 
-    
-    
-    
+
+document.getElementById("save-button").addEventListener("click", function () {
+    savingAmount();
 });
