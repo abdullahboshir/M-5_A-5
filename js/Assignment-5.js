0fc95629f79d6bd3b96797ec3390172c82a1d9f7
abdullahboshir <abdullahboshir@gmao.com>
2022-02-21 22:56:29 +0600

    second commit

--- js/Assignment-5.js
+++ js/Assignment-5.js
@@ -1,12 +1,40 @@
+function moneyMaster() {
+    const foodInput = document.getElementById("food-cost").value;
+    const foodCostNumber = parseInt(foodInput);
+}
+
+
+
+
 document.getElementById("calculation-btn").addEventListener("click", function () {
-    const foodCost = document.getElementById("food-cost").value;
-    const foodCostNumber = parseInt(foodCost);
-    const rentCost = document.getElementById("rent-cost").value;
-    const rentCostNumber = parseInt(rentCost);
-    const clothesCost = document.getElementById("clothes-cost").value;
-    const clothesCostNumber = parseInt(clothesCost);
-    const totalExpenses = foodCostNumber + rentCostNumber + clothesCostNumber;
+    const foodInput = document.getElementById("food-cost").value;
+    const foodCostNumber = parseInt(foodInput);
+    const rentInput = document.getElementById("rent-cost").value;
+    const rentCostNumber = parseInt(rentInput);
+    const clothesInput = document.getElementById("clothes-cost").value;
+    const clothesCostNumber = parseInt(clothesInput);
+    const total = foodCostNumber + rentCostNumber + clothesCostNumber;
+    
+
+    const balance = document.getElementById("total-balance").innerText = 10000 - total;
+
+
+    if (total > 0) {
+        const totalExpensesText = document.getElementById("total-expenses").innerText = total; 
+    }
+});
+
+
+
+
+document.getElementById("save-button").addEventListener("click", function () {
+    const savingInput = document.getElementById("saving-input").value;
+    const savingAmount = document.getElementById("saving-amount").innerText = 10000 / 100 * savingInput;
+
+    // remaining balance 
+    const remainingBalance = document.getElementById("remaining-balance").innerText = 10000 - savingAmount;
 
     
-   
+    
+    
 });
