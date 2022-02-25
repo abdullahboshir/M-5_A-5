596de821d4c71eee9d185cc4e206b795edf826ba
abdullahboshir <abdullahboshir@gmao.com>
2022-02-25 22:51:33 +0600

    fourth commit

--- js/Assignment-5.js
+++ js/Assignment-5.js
@@ -1,38 +1,94 @@
-function moneyMaster(item) {
-    const foodInput = document.getElementById(item + "-cost").value;
-    itemCost = parseInt(foodInput);
-    const total = itemCost + itemCost + itemCost;
-
-
-    if (total > 0) {
-        const totalExpensesText = document.getElementById("total-expenses").innerText = total; 
-    }
-    return balance = document.getElementById("total-balance").innerText = 10000 - total;
+function calculate(recieveAllId) {
+    return document.querySelector(recieveAllId);
 };
 
 
+const totalIncome = calculate("#total-income");
+const foodInput = calculate("#food-input");
+const rentInput = calculate("#rent-input");
+const clothesInput = calculate("#clothes-input");
+const totalExpense = calculate("#total-expenses");
+const totalBalance = calculate("#total-balance");
+
+const calculateBtn = calculate("#calculation-btn");
+const saveInput = calculate("#saving-input");
+const saveBtn = calculate("#save-button");
+const savingAmount = calculate("#saving-amount");
+const remainingBalance = calculate("#remaining-balance");
 
 
-document.getElementById("calculation-btn").addEventListener("click", function () {
-    
-    moneyMaster("food");
-    moneyMaster("rent");
-    moneyMaster("clothes");
+
+
+function expenses(food, rent, clothes) {
+    return parseInt(food) + parseInt(rent) + parseInt(clothes);
+};
+
+
+calculateBtn.addEventListener("click", function () {
+    const IncomeTotal = parseInt(totalIncome.value);
+    const expensesTotal = expenses(foodInput.value, rentInput.value, clothesInput.value);
+    const balance = IncomeTotal - expensesTotal;
+
 
    
+
+    if (IncomeTotal > expensesTotal) {
+        totalBalance.innerText = balance;
+        totalExpense.innerText = expensesTotal;
+    }
+    else if ( IncomeTotal < expensesTotal) {
+        alert("your balance is low")
+    }
+    else if (foodInput.value == "" && rentInput.value == "" && clothesInput.value == "") {
+        alert("Your Total Input or Expense is blank!")   
+    }
+    else{
+        alert("You have no income"); 
+    }
 });
 
 
-function savingAmount() {
-    const savingInput = document.getElementById("saving-input").value;
-    const savingAmount = document.getElementById("saving-amount").innerText = 10000 / 100 * savingInput;
 
-    // remaining balance 
-    const remainingBalance = document.getElementById("remaining-balance").innerText = savingAmount;
-    
+
+function persent(divided) {
+    const IncomeTotall = parseInt(totalIncome.value);
+    const dividedNumber = parseInt(divided);
+    return IncomeTotall * dividedNumber / 100;    
 };
 
 
-document.getElementById("save-button").addEventListener("click", function () {
-    savingAmount();
+function remaining(add) {
+    const IncomeTotall = parseInt(totalIncome.value);
+    const intotalExpenses = parseInt(add);
+    return IncomeTotall - intotalExpenses;    
+};
+
+
+saveBtn.addEventListener("click", function () {
+    const saveMoney = persent(saveInput.value);
+    savingAmount.innerText = saveMoney;
+    
+
+    const expensesTotall = expenses(foodInput.value, rentInput.value, clothesInput.value);
+
+    const intotalExpenses = saveMoney + expensesTotall;
+    const totalExpensePlusSabving = remaining(intotalExpenses)
+   
+   
+   
+    const IncomeTotal = parseInt(totalIncome.value);
+
+    if (intotalExpenses < IncomeTotal) {
+        remainingBalance.innerText = totalExpensePlusSabving;
+    }
+    else {
+        alert("your total income is low")
+   }
 });
+
+
+
+
+
+
+
